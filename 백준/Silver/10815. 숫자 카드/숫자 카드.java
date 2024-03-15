3e3437e3a3bba8ca1f91506751801c93ba868c42
BrothersJJ <144659958+BrothersJJ@users.noreply.github.com>
2024-03-15 13:49:43 +0900

    [Silver V] Title: 숫자 카드, Time: 3572 ms, Memory: 136388 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -14,6 +14,7 @@ public class Main {
             getArray[i] = getHave;
         }
 
+        // 이진 탐객을 위한 정렬
         Arrays.sort(getArray);
 
         int checkCount = Integer.parseInt(br.readLine());
@@ -21,11 +22,16 @@ public class Main {
 
         for (int i = 0; i < checkCount; i++) {
             int checkHave = Integer.parseInt(st.nextToken());
+            // getArrat와 checkHave를 이진탐색하여 boolean 얻기
             boolean found = binarySearch(getArray, checkHave);
+            // true면 1, 아니면 0
             System.out.print(found ? "1 " : "0 ");
         }
     }
 
+    // 이진탐색, 중앙에서 비교
+    // static은 해당 메서드가 객체의 인스턴스에 속하지 않고 클래스에 직접 속함
+    // 이는 해당 메서드를 객체의 생성 없이도 호출
         private static boolean binarySearch(int[] arr, int target) {
             int left = 0;
             int right = arr.length - 1;
@@ -42,6 +48,7 @@ public class Main {
                 }
             }
 
+            // 같은게 없으면 false
             return false;
     }
 }
