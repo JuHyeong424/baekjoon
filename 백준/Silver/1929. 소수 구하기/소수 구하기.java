44e03f29a911092f337997934b3840e20a70cb7c
BrothersJJ <144659958+BrothersJJ@users.noreply.github.com>
2024-03-24 20:31:51 +0900

    [Silver III] Title: 소수 구하기, Time: 848 ms, Memory: 32444 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -7,29 +7,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long m = Long.parseLong(st.nextToken());
-        long n = Long.parseLong(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-        for (long i = m; i <= n; i++) {
-            if (is_prime(i)) {
+        boolean[] prime = new boolean[m + 1];
+
+        prime[0] = prime[1] = true;
+
+        // 이미 true이면 넘기기 ex) 4, 6
+        for (int i = 2; i <= Math.sqrt(prime.length); i++) {
+            if (prime[i]) {
+                continue;
+            }
+
+            // j = i * 2가 아닌 이유는 2배수에 걸러졌기 때문
+            // 따라서 제곱수부터 시작
+            // i만큼 증가
+            for (int j = i * i; j < prime.length; j = j + i) {
+                prime[j] = true;
+            }
+        }
+
+        for (int i = n; i <= m; i++) {
+            if (!prime[i]) {
                 System.out.println(i);
             }
         }
-    }
-    // 소수 찾기
-    // 소수가 아니면 false
-    private static boolean is_prime(long a) {
-        if (a < 2) {
-            return false;
-        } if (a == 2) {
-            return true;
-        }
-
-        for (int i = 2; i <= Math.sqrt(a); i++) {
-            if (a % i == 0) {
-                return false;
-            }
-        }
-        return true;
     }
 }
