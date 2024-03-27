d0e34cad470706163f888950539f992a74b4d13d
BrothersJJ <144659958+BrothersJJ@users.noreply.github.com>
2024-03-27 13:30:08 +0900

    [Silver I] Title: 카드 구매하기, Time: 152 ms, Memory: 14532 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        // arr: n개를 사기 위한 비용
+        int[] arr = new int[n+1];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // dp: n개를 사기 위한 최대 비용
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+
+        // dp[i - j] + arr[j]
+        // i - j개 구하는 최대가격 + j개 비용
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.max(dp[i], dp[i - j] + arr[j]);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
