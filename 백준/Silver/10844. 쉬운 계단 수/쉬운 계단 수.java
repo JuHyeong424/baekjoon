51c2385961373d8dcb70c9a2b43183d9e66fb85e
BrothersJJ <144659958+BrothersJJ@users.noreply.github.com>
2024-03-27 21:58:56 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -11,36 +11,33 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
 
+        // 쉽게 보기 위해 n + 1
         dp = new long[n + 1][10];
 
+        // 한 자리는 경우의 수가 1밖에 없으므로 1로 초기화
         for (int i = 0; i < 10; i++) {
             dp[1][i] = 1;
         }
 
-        long count = 0;
-
-        for (int i = 1; i <= 9; i++) {
-            count += stairs(n, i);
-        }
-
-        System.out.println(count % m);
-    }
-
-    public static long stairs(int a, int b) {
-        if (a == 1) {
-            return dp[1][b];
-        }
-
-        if (dp[a][b] == 0) {
-            if (b == 0) {
-                dp[a][b] = stairs(a - 1, 1);
-            } else if (b == 9) {
-                dp[a][b] = stairs(a - 1, 8);
-            } else {
-                dp[a][b] = stairs(a - 1, b - 1) + stairs(a - 1, b + 1);
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][1] % m;
+                } else if (j == 9) {
+                    dp[i][j] = dp[i - 1][8] % m;
+                } else{
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % m;
+                }
             }
         }
 
-        return dp[a][b] % m;
+        long count = 0;
+
+        // 마지막 자릿수 1 ~ 9까지의 경우의 수 모두 더함
+        for (int i = 1; i <= 9; i++) {
+            count += dp[n][i];
+        }
+
+        System.out.println(count % m);
     }
 }
