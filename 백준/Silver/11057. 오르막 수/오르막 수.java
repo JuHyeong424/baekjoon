25666452b60aeed663fe9845e3ab1f8e2950dd14
BrothersJJ <144659958+BrothersJJ@users.noreply.github.com>
2024-03-30 21:05:27 +0900

    [Silver I] Title: 오르막 수, Time: 140 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/11057. 오르막 수/오르막 수.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[n + 1][10];
+        int m = 10007;
+
+        for (int j = 0; j < 10; j++) {
+            dp[1][j] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            dp[i][9] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 8; j >= 0; j--) {
+                dp[i][j] = (dp[i][j + 1] + dp[i - 1][j]) % m;
+            }
+        }
+
+        int sum = 0;
+        for (int j = 0; j < 10; j++) {
+            sum += dp[n][j];
+        }
+
+        System.out.println(sum % m);
+    }
+}
