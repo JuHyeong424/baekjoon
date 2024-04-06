68112115ef6cb356bfc3d2d11c15dcef86abc0bc
BrothersJJ <144659958+BrothersJJ@users.noreply.github.com>
2024-04-06 21:45:11 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[9];
+        int sum = 0;
+
+        for (int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        Arrays.sort(arr);
+
+        int a = 0;
+        int b = 0;
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - (arr[i] + arr[j]) == 100) {
+                    a = i;
+                    b = j;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (i == a || i == b) continue;
+            System.out.println(arr[i]);
+        }
+    }
+}
