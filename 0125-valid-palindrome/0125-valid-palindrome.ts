df85a2b286f2b5f7cdbf3d47f2070ec77f45a7c7
JuHyeong424 <144659958+JuHyeong424@users.noreply.github.com>
2026-03-15 15:18:48 +0900

    Time: 6 ms (48.2%), Space: 61.4 MB (32.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,6 +1,6 @@
 function isPalindrome(s: string): boolean {
     const regexString = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
-    const reverse = regexString.split("").reverse().join("");
+    const reverse = [...regexString].reduce((rev, char) => char + rev, '');
     if (regexString === reverse) return true;
     return false;
 };
