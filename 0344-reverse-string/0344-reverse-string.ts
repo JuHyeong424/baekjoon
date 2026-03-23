dab4850b1cce8121255a897ec4953a2f7b8e2b47
JuHyeong424 <144659958+JuHyeong424@users.noreply.github.com>
2026-03-23 21:02:10 +0900

    Time: 24 ms (6.05%), Space: 66.5 MB (5.03%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.ts
+++ 0344-reverse-string/0344-reverse-string.ts
@@ -2,6 +2,18 @@
  Do not return anything, modify s in-place instead.
  */
 function reverseString(s: string[]): void {
-    const reverseArray = s.reverse().join('');
-    console.log(reverseArray);
+    let left = 0;
+    let right = s.length - 1;
+    let empty = "";
+
+    while (left < right) {
+        empty = s[right];
+        s[right] = s[left];
+        s[left] = empty;
+
+        left++;
+        right--;
+    }
+
+    console.log(s);
 };
