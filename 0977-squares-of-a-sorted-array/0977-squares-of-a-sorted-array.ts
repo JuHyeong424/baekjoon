a82a0a6b7d8ad06104823ce8bbd6a09ec5864813
JuHyeong424 <144659958+JuHyeong424@users.noreply.github.com>
2026-03-26 23:16:15 +0900

    Time: 4 ms (70.51%), Space: 62.4 MB (91.22%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.ts
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.ts
@@ -1,17 +1,17 @@
 function sortedSquares(nums: number[]): number[] {
-    for (let i = 0; i < nums.length; i++) {
-        nums[i] *= nums[i];
-    }
-    const newArray = [...nums];
+    const newArray = new Array(nums.length);
     let p1 = 0; 
     let p2 = nums.length - 1;
 
     for (let i = newArray.length - 1; i >= 0; i--) {
-        if (nums[p1] > nums[p2]) {
-            newArray[i] = nums[p1];
+        const left = nums[p1] * nums[p1];
+        const right = nums[p2] * nums[p2];
+
+        if (left > right) {
+            newArray[i] = left;
             p1++;
         } else {
-            newArray[i] = nums[p2];
+            newArray[i] = right;
             p2--;
         }
     }
