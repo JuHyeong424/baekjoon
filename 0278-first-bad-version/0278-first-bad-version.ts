f6fb83c4569df5ac9ef74473f0b3e5a269ff0b57
JuHyeong424 <144659958+JuHyeong424@users.noreply.github.com>
2026-04-07 21:29:49 +0900

    Time: 47 ms (31.45%), Space: 54.9 MB (71.17%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.ts
+++ 0278-first-bad-version/0278-first-bad-version.ts
@@ -11,7 +11,7 @@ var solution = function(isBadVersion: any) {
         let left = 1;
         let right = n;
 
-        while (left <= right) {
+        while (true) {
             const mid = Math.floor(left + (right - left) / 2);
 
             if (isBadVersion(mid) && !isBadVersion(mid - 1)) return mid;
